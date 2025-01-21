45d6e68c0d65995d0da9e5959a3bbe6675d7641b
abhash157 <limbuabhash000@gmail.com>
2025-01-21 15:12:15 +0545

    added player controls

--- app.js
+++ app.js
@@ -11,31 +11,60 @@ serv.listen(2000);
 console.log("Server started.");
 
 var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = (id) => {
+  var self = {
+    x: 250,
+    y: 250,
+    id: id,
+    number: "" + Math.floor(Math.random() * 10),
+    pressingRight: false,
+    pressingLeft: false,
+    pressingUp: false,
+    pressingDown: false,
+    maxSpd: 10,
+  };
+  self.updatePosition = () => {
+    if (self.pressingLeft) self.x -= self.maxSpd;
+    if (self.pressingRight) self.x += self.maxSpd;
+    if (self.pressingUp) self.y -= self.maxSpd;
+    if (self.pressingDown) self.y += self.maxSpd;
+  };
+  return self;
+};
 
 var io = require("socket.io")(serv, {});
 io.sockets.on("connection", function (socket) {
-  console.log("socket connection");
-
   socket.id = Math.random();
-  socket.x = 0;
-  socket.y = 0;
-  socket.number = "" + Math.floor(Math.random() * 10);
   SOCKET_LIST[socket.id] = socket;
+
+  var player = Player(socket.id);
+  PLAYER_LIST[socket.id] = player;
 
   socket.on("disconnect", () => {
     delete SOCKET_LIST[socket.id];
+    delete PLAYER_LIST[socket.id];
+  });
+
+  socket.on("keyPress", function (data) {
+    console.log(data.inputId + "  " + data.state);
+    if (data.inputId == "left") player.pressingLeft = data.state;
+    else if (data.inputId == "right") player.pressingRight = data.state;
+    else if (data.inputId == "up") player.pressingUp = data.state;
+    else if (data.inputId == "down") player.pressingDown = data.state;
   });
 });
+
 setInterval(() => {
   var pack = [];
-  for (var i in SOCKET_LIST) {
-    socket = SOCKET_LIST[i];
-    socket.x++;
-    socket.y++;
+  for (var i in PLAYER_LIST) {
+    var player = PLAYER_LIST[i];
+    player.updatePosition();
     pack.push({
-      x: socket.x,
-      y: socket.y,
-      number: socket.number,
+      x: player.x,
+      y: player.y,
+      number: player.number,
     });
   }
   for (var i in SOCKET_LIST) {
